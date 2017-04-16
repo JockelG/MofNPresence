ecde3721050b7d2e63c0d733886e467eaea6ab0f
Rob Dailey <rob@suspected.org>
2017-04-15 21:51:05 -0400

    Changed a ton.  Next update will move away from a counter and just check the list at each event change to prevent any race conditions.

--- smartapps/pahrohfit/mofn-presence.src/mofn-presence.groovy
+++ smartapps/pahrohfit/mofn-presence.src/mofn-presence.groovy
@@ -1,7 +1,7 @@
 /**
- *  Reliable Presence
+ *  MofN Presence
  *
- *  Copyright 2015 Brian Gudauskas
+ *  Copyright 2017 Rob Dailey
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
@@ -14,12 +14,13 @@
  *
  * 04/17 Forked version for reliable presence
  */
+ 
 definition(
     name: "MofN Presence",
     namespace: "pahrohfit",
     author: "pah roh fit",
     description: "Will set a simulated presence sensor based on MofN arrival/departure.",
-    category: "Fun & Social",
+    category: "Safety & Security",
     iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
     iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
     iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
@@ -28,62 +29,60 @@ definition(
 preferences {
 	section("Select Presence Sensor Group") {
 	input "presenceSensors", "capability.presenceSensor", title: "Presence Sensors Required", multiple: true, required: true, submitOnChange: true
-	input "presenceSensorsMofN", "number", title: "Number Required (the M of the N)", multiple: false, required: true, defaultValue: "2" , range: "2..presenceSensors.values.size()", hideWhenEmpty: "presenceSensors"
-        input "simulatedPresence", "device.simulatedPresenceSensor", title: "Simulated Presence Sensor", multiple: false, required: true
+	input "presenceSensorsMofN", "number", title: "Number Required (the M of the N)", multiple: false, required: true, range: "2..presenceSensors.size()", hideWhenEmpty: "presenceSensors"
+    input "simulatedPresence", "device.simulatedPresenceSensor", title: "Simulated Presence Sensor", multiple: false, required: true
 	}
 }
 
 def installed() {
 	log.debug "Installed with settings: ${settings}"
-	initialize()
+    initialize()
 }
 
 def updated() {
 	log.debug "Updated with settings: ${settings}"
 	unsubscribe()
-	initialize()
+    initialize()
 }
 
 def initialize() {
-	setPresence()
-	subscribe(presenceSensors, "presence", "presenceHandler")
+    state.presentCntr = 0
+    presenceSensors.each { 
+      log.debug("${it.label ?: it.name} -> ${it.currentValue("presence")}")
+      if (it.currentValue("presence") == "present"){
+        state.presentCntr = state.presentCntr + 1
+      } else if (it.currentValue("presence") == "not present"){
+        state.presentCntr = state.presentCntr - 1
+      }
+    }
+    updatePresence()
+    log.debug("presenceCounter: ${state.presentCntr}, simulatedPresence: ${simulatedPresence.currentValue("presence")}")
+    subscribe(presenceSensors, "presence", presenceHandler)
 }
 
 def presenceHandler(evt) {
-
+    log.debug("new event ${evt}")
     if (evt.value == "present") {
-        if (simulatedPresence.currentValue("presence") != "present") {
-            simulatedPresence.arrived()
-            log.debug("Reliable Arrival")
-            }
+        state.presentCntr = state.presentCntr + 1
     } else if (evt.value == "not present") {
-        if (simulatedPresence.currentValue("presence") != "not present") {
-            simulatedPresence.departed()
-            log.debug("Reliable Departure")
-        }
-    }
-}
+        state.presentCntr = state.presentCntr - 1
+    } else { log.debug("Unknown event value -> ${evt.value}") }
+  	log.debug("MofN Testing Trigger Event ${evt.displayName} : ${evt.device} : ${evt.name} : ${evt.value}")
+    updatePresence()
+} 
 
-def setPresence(){
-	def presentCounter = 0
 
-    presenceSensors.each {
-    	if (it.currentValue("presence") == "present") {
-        	presentCounter++
-        }
-    }
-
-    log.debug("presentCounter: ${presentCounter}, simulatedPresence: ${simulatedPresence.currentValue("presence")}")
-
-    if (presentCounter == presenceSensorsMofN) {
+def updatePresence() {
+    if (state.presentCntr >= presenceSensorsMofN) {
     	if (simulatedPresence.currentValue("presence") != "present") {
-    		simulatedPresence.arrived()
-            log.debug("MofN Arrival")
+        	simulatedPresence.arrived()
+            log.debug("MofN Arrival ${state.presentCntr}/${presenceSensorsMofN}")
         }
     } else {
-    	if (simulatedPresence.currentValue("presence") != "not present") {
-    		simulatedPresence.departed()
-            log.debug("MofN Departure")
+        if (simulatedPresence.currentValue("presence") != "not present") {
+            simulatedPresence.departed()
+            log.debug("MofN Departure ${state.presentCntr}/${presenceSensorsMofN}")
         }
     }
 }
+
