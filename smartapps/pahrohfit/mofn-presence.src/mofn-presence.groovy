eb0832a0a746cb587838dce9da3d2b7c1a9ac488
Rob Dailey <rob@suspected.org>
2017-04-17 17:35:11 -0400

    v1.0

--- smartapps/pahrohfit/mofn-presence.src/mofn-presence.groovy
+++ smartapps/pahrohfit/mofn-presence.src/mofn-presence.groovy
@@ -19,23 +19,63 @@ definition(
     name: "MofN Presence",
     namespace: "pahrohfit",
     author: "pah roh fit",
-    description: "Will set a simulated presence sensor based on MofN arrival/departure.",
+    description: "Will set a simulated presence sensor based on MofN presence.",
     category: "Safety & Security",
-    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
-    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
-    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
+    iconUrl: "https://s3.amazonaws.com/smartapp-icons/SafetyAndSecurity/Cat-SafetyAndSecurity.png",
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/SafetyAndSecurity/Cat-SafetyAndSecurity@2x.png",
+    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/SafetyAndSecurity/Cat-SafetyAndSecurity@2x.png")
 
 
 preferences {
-	section("Select Presence Sensor Group") {
-	input "presenceSensors", "capability.presenceSensor", title: "Presence Sensors Required", multiple: true, required: true, submitOnChange: true
-	input "presenceSensorsMofN", "number", title: "Number Required (the M of the N)", multiple: false, required: true, range: "2..presenceSensors.size()", hideWhenEmpty: "presenceSensors"
-    input "simulatedPresence", "device.simulatedPresenceSensor", title: "Simulated Presence Sensor", multiple: false, required: true
+    page(name: "virtSensDef", title: "Virtual Sensor Definition", uninstall: true, install: false, nextPage: "mofnSetup"){
+		section("Select Presence Sensor Group") {
+			input "presenceSensors", "capability.presenceSensor", title: "Presence Sensors Required", multiple: true, required: true
+         }
+		section("Select Virtual Presence Sensor to Update") {
+        	paragraph("This will update a virtual presence sensor of status, based on meeting a threshold of linked presence devices.  You must have added a virtual presence sensor in your IDE for this to work!")
+    		input "simulatedPresence", "device.simulatedPresenceSensor", title: "Simulated Presence Sensor", multiple: false, required: true
+   		}
+    }
+    page(name: "mofnSetup", title: "M of N Setup", install: false, uninstall: true, nextPage: "stSettings") 	
+    page(name: "stSettings", title: "SmartThings Settings", install: true, uninstall: true){
+        section(name: "smartSettings", title: "Smart Settings"){
+        	 mode(name: "modeMultiple", title: "Which Modes To Run Under", required: false)
+             label(name: "label", title: "Name for this M of N Instance", required: false, multiple: false) 
+        }
+        section("Send Notifications?") {
+            input("recipients", "contact", title: "Send notifications to") {
+            	input("phone", "phone", title: "Warn with text message (optional)", description: "Phone Number", required: false)
+       		}
+            input("sendPush", "bool", required: false, title: "Send Push Notifications on Change?")
+    	}
+     }
+}
+
+def mofnSetup(){
+	dynamicPage(name: "mofnSetup") {
+    	section() {
+        	paragraph "How many devices (M) of the full set of selected devices (N) do you require for the presense to be 'true'?"
+   			input("presenceSensorsMofN", "enum", title: "Number Required (the M of the N)", options: mofnRange(), defaultValue: mofnRange("mofn"), required: true)
+   		}
 	}
 }
 
+def mofnRange(action){
+	def cntr = 1
+	def mofnRangeValues = []    
+   	presenceSensors.each(){
+        mofnRangeValues.add("${cntr}")
+        cntr += 1
+    }
+    log.debug("settings panel options: ${mofnRangeValues}")
+    if (action == "mofn") { log.debug("returning mofn: ${presenceSensorsMofN ?: cntr}"); return presenceSensorsMofN ?: cntr }
+    else { return mofnRangeValues }
+}
+    
+
 def installed() {
 	log.debug "Installed with settings: ${settings}"
+	unsubscribe()
     initialize()
 }
 
@@ -48,41 +88,59 @@ def updated() {
 def initialize() {
     state.presentCntr = 0
     presenceSensors.each { 
-      log.debug("${it.label ?: it.name} -> ${it.currentValue("presence")}")
+      log.debug("initialization ${it.label ?: it.name} -> ${it.currentValue("presence")}")
       if (it.currentValue("presence") == "present"){
-        state.presentCntr = state.presentCntr + 1
+        state.presentCntr = state.presentCntr + 1  // add it
       } else if (it.currentValue("presence") == "not present"){
-        state.presentCntr = state.presentCntr - 1
+        state.presentCntr = state.presentCntr + 0  // do nothing
       }
     }
     updatePresence()
-    log.debug("presenceCounter: ${state.presentCntr}, simulatedPresence: ${simulatedPresence.currentValue("presence")}")
+    log.info("initialization presenceCounter: ${state.presentCntr}, simulatedPresence: ${simulatedPresence.currentValue("presence")}")
     subscribe(presenceSensors, "presence", presenceHandler)
 }
 
 def presenceHandler(evt) {
-    log.debug("new event ${evt}")
+    log.debug("new event found: ${evt}")
+  	log.debug("MofN testing trigger event ${evt.displayName}-> ${evt.name}:${evt.value}, [${state.presentCntr}/${presenceSensorsMofN}]")
     if (evt.value == "present") {
         state.presentCntr = state.presentCntr + 1
     } else if (evt.value == "not present") {
         state.presentCntr = state.presentCntr - 1
-    } else { log.debug("Unknown event value -> ${evt.value}") }
-  	log.debug("MofN Testing Trigger Event ${evt.displayName} : ${evt.device} : ${evt.name} : ${evt.value}")
+    } else { log.error("unknown event value -> ${evt.value}") }
     updatePresence()
 } 
 
-
 def updatePresence() {
-    if (state.presentCntr >= presenceSensorsMofN) {
+    log.debug("simulatedPresence ${simulatedPresence.currentValue("presence")}")
+    if (state.presentCntr.toInteger() >= presenceSensorsMofN.toInteger()) {
+        log.debug("updatePresence >= '${state.presentCntr}' | '${presenceSensorsMofN}'")
     	if (simulatedPresence.currentValue("presence") != "present") {
         	simulatedPresence.arrived()
-            log.debug("MofN Arrival ${state.presentCntr}/${presenceSensorsMofN}")
-        }
+            sendNotice("Present")
+            log.info("MofN Arrival ${state.presentCntr}/${presenceSensorsMofN}")
+        } else { log.debug("MofN reached, but state change not necessary") }
     } else {
+        log.debug("updatePresence < '${state.presentCntr}' | '${presenceSensorsMofN}'")
         if (simulatedPresence.currentValue("presence") != "not present") {
             simulatedPresence.departed()
-            log.debug("MofN Departure ${state.presentCntr}/${presenceSensorsMofN}")
-        }
+            sendNotice("Not Present")
+            log.info("MofN Departure ${state.presentCntr}/${presenceSensorsMofN}")
+        } else { log.debug("MofN is *not* reached, but state change not necessary") }
     }
 }
 
+def sendNotice(vsState) {
+    log.debug("sendNotice called.")
+	// check that Contact Book is enabled and recipients selected
+	if (location.contactBookEnabled && recipients) {
+    	sendNotificationToContacts("${simulatedPresence.displayName} state changed to '${vsState}'", recipients)
+	} else if (phone) { // check that the user did select a phone number
+    	sendSms(phone, "${simulatedPresence.displayName} state changed to '${vsState}'")
+	} else { log.debug("sendNotice called.  no email/sms config defined.") }
+    
+    // send push notifications if defined
+    if (sendPush) {
+        sendPush("${simulatedPresence.displayName} state changed to '${vsState}'")
+	} else { log.debug("sendNotice called.  no email/sms config defined.") }
+}
